7468497fb7fdc1585d5364deb4ff8297d2db28c2
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-06 20:13:51 +0200

    feat(dao): global settings

---
+++ work_time_table_mobile/lib/daos/global_setting_dao.dart
@@ -0,0 +1,61 @@
+import 'package:orm/orm.dart';
+import 'package:work_time_table_mobile/_generated_prisma_client/prisma.dart';
+import 'package:work_time_table_mobile/models/global_setting_key.dart';
+import 'package:work_time_table_mobile/models/settings_map.dart';
+import 'package:work_time_table_mobile/prisma.dart';
+import 'package:work_time_table_mobile/streamed_dao_helpers/dao_stream.dart';
+import 'package:work_time_table_mobile/streamed_dao_helpers/streamable_dao.dart';
+
+final _stream = DaoStream<SettingsMap>({});
+
+class GlobalSettingDao implements StreamableDao<SettingsMap> {
+  const GlobalSettingDao();
+
+  Future<void> loadUserValues(int userId) async {
+    final settings = await prisma.globalSetting.findMany(
+      where: GlobalSettingWhereInput(userId: PrismaUnion.$2(userId)),
+    );
+    _stream.emitReload(Map.fromIterable(settings.map((setting) => MapEntry(
+          GlobalSettingKey.values.firstWhere((d) => d.name == setting.key),
+          setting.value,
+        ))));
+  }
+
+  Future<void> updateByUserIdAndKey(
+      int userId, GlobalSettingKey key, String? value) async {
+    if (value == null) {
+      await prisma.globalSetting.delete(
+        where: GlobalSettingWhereUniqueInput(
+            userIdKey: GlobalSettingUserIdKeyCompoundUniqueInput(
+          userId: userId,
+          key: key.name,
+        )),
+      );
+      await loadUserValues(userId);
+    } else {
+      await prisma.globalSetting.upsert(
+        where: GlobalSettingWhereUniqueInput(
+            userIdKey: GlobalSettingUserIdKeyCompoundUniqueInput(
+          userId: userId,
+          key: key.name,
+        )),
+        create: PrismaUnion.$1(GlobalSettingCreateInput(
+          key: key.name,
+          value: value,
+          user: UserCreateNestedOneWithoutGlobalSettingInput(
+            connect: UserWhereUniqueInput(id: userId),
+          ),
+        )),
+        update: PrismaUnion.$1(GlobalSettingUpdateInput(
+          value: PrismaUnion.$1(value),
+        )),
+      );
+      await loadUserValues(userId);
+    }
+  }
+
+  @override
+  SettingsMap get data => _stream.state;
+  @override
+  Stream<SettingsMap> get stream => _stream.stream;
+}
