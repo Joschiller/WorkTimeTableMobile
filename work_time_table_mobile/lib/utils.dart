36ab9cc033661a3ffdea6fc34aaf523b62fb5cc1
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-17 16:58:35 +0200

    feat(utils): validateAndRun

--- work_time_table_mobile/lib/utils.dart
+++ work_time_table_mobile/lib/utils.dart
@@ -1,4 +1,19 @@
+import 'package:work_time_table_mobile/app_error.dart';
+
 extension IsBlank on String {
   bool get isBlank => trim().isEmpty;
   bool get isNotBlank => trim().isNotEmpty;
+}
+
+Future<void> validateAndRun(
+  List<AppError? Function()> validations,
+  Future<void> Function() action,
+) async {
+  for (final v in validations) {
+    final err = v();
+    if (err != null) {
+      return Future.error(err);
+    }
+  }
+  return action();
 }
