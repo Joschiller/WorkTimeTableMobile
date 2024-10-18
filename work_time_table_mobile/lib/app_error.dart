415625d4c496a8088405bd20047cb75b22f04c56
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-18 17:37:12 +0200

    feat: WeekSettingsService

--- work_time_table_mobile/lib/app_error.dart
+++ work_time_table_mobile/lib/app_error.dart
@@ -3,11 +3,15 @@
 enum AppError {
   data_dao_unknownUser('Tried to perform an action for an unknown user.'),
 
+  service_noUserLoaded('No user is loaded yet.'),
+
   service_user_unknownUser('Tried to perform an action for an unknown user.'),
   service_user_invalidName('This name is invalid.'),
   service_user_duplicateName('This name is already used.'),
   service_user_forbiddenDeletion('Deletion of this user is not allowed.'),
   service_user_unconfirmedDeletion('Deleting the user was not confirmed.'),
+
+  service_weekSettings_invalid('The week settings are invalid.'),
   ;
 
   final String displayText;
---
+++ work_time_table_mobile/lib/services/week_settings_service.dart
@@ -0,0 +1,97 @@
+import 'package:work_time_table_mobile/app_error.dart';
+import 'package:work_time_table_mobile/daos/current_user_dao.dart';
+import 'package:work_time_table_mobile/daos/week_setting_dao.dart';
+import 'package:work_time_table_mobile/models/week_setting/week_setting.dart';
+import 'package:work_time_table_mobile/streamed_dao_helpers/user_dependent_value.dart';
+import 'package:work_time_table_mobile/utils.dart';
+
+class WeekSettingsService {
+  WeekSettingsService(this.currentUserDao, this.weekSettingDao) {
+    currentUserDao.stream.listen((selectedUser) => runUserDependentAction(
+          selectedUser,
+          () => _loadData(null),
+          (user) => _loadData(user.id),
+        ));
+  }
+
+  final CurrentUserDao currentUserDao;
+  final WeekSettingDao weekSettingDao;
+
+  Future<void> _loadData(int? userId) =>
+      weekSettingDao.loadUserSettings(userId);
+
+  Future<void> updateWeekSettings(WeekSetting settings) =>
+      runUserDependentAction(
+        currentUserDao.data,
+        () async => Future.error(AppError.service_noUserLoaded),
+        (user) => validateAndRun([
+          // targetWorkTimePerWeek <= SUM(timeEquivalent)
+          () => settings.targetWorkTimePerWeek >
+                  settings.weekDaySettings.values
+                      .map((s) => s.timeEquivalent)
+                      .reduce((a, b) => a + b)
+              ? AppError.service_weekSettings_invalid
+              : null,
+          // each day of week is unique
+          () => settings.weekDaySettings.entries
+                  .any((day) => day.key != day.value.dayOfWeek)
+              ? AppError.service_weekSettings_invalid
+              : null,
+          // (start == null) == (end == null)
+          () => settings.weekDaySettings.values.any((day) =>
+                  (day.defaultWorkTimeStart == null) !=
+                  (day.defaultWorkTimeEnd == null))
+              ? AppError.service_weekSettings_invalid
+              : null,
+          () => settings.weekDaySettings.values.any((day) =>
+                  (day.mandatoryWorkTimeStart == null) !=
+                  (day.mandatoryWorkTimeEnd == null))
+              ? AppError.service_weekSettings_invalid
+              : null,
+          // start <= end
+          () => settings.globalWeekDaySetting.defaultWorkTimeStart >
+                  settings.globalWeekDaySetting.defaultWorkTimeEnd
+              ? AppError.service_weekSettings_invalid
+              : null,
+          () => settings.globalWeekDaySetting.defaultMandatoryWorkTimeStart >
+                  settings.globalWeekDaySetting.defaultMandatoryWorkTimeEnd
+              ? AppError.service_weekSettings_invalid
+              : null,
+          () => settings.weekDaySettings.values.any((day) =>
+                  (day.defaultWorkTimeStart ?? 0) >
+                  (day.defaultWorkTimeEnd ?? 0))
+              ? AppError.service_weekSettings_invalid
+              : null,
+          () => settings.weekDaySettings.values.any((day) =>
+                  (day.mandatoryWorkTimeStart ?? 0) >
+                  (day.mandatoryWorkTimeEnd ?? 0))
+              ? AppError.service_weekSettings_invalid
+              : null,
+          // default time within manatory time
+          () => settings.globalWeekDaySetting.defaultWorkTimeStart >
+                  settings.globalWeekDaySetting.defaultMandatoryWorkTimeStart
+              ? AppError.service_weekSettings_invalid
+              : null,
+          () => settings.globalWeekDaySetting.defaultWorkTimeEnd <
+                  settings.globalWeekDaySetting.defaultMandatoryWorkTimeEnd
+              ? AppError.service_weekSettings_invalid
+              : null,
+          () => settings.weekDaySettings.values.any((day) =>
+                  (day.defaultWorkTimeStart ??
+                      settings.globalWeekDaySetting.defaultWorkTimeStart) >
+                  (day.mandatoryWorkTimeStart ??
+                      settings
+                          .globalWeekDaySetting.defaultMandatoryWorkTimeStart))
+              ? AppError.service_weekSettings_invalid
+              : null,
+          () => settings.weekDaySettings.values.any((day) =>
+                  (day.defaultWorkTimeEnd ??
+                      settings.globalWeekDaySetting.defaultWorkTimeEnd) <
+                  (day.mandatoryWorkTimeEnd ??
+                      settings
+                          .globalWeekDaySetting.defaultMandatoryWorkTimeEnd))
+              ? AppError.service_weekSettings_invalid
+              : null,
+        ], () => weekSettingDao.updateByUserId(user.id, settings)),
+      );
+}
