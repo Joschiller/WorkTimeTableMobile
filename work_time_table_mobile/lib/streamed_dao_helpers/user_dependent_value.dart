e7ffd02ca64ccc73f1d84cdf7d94e9b9fe983c64
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-18 17:10:45 +0200

    feat: runUserDependentAction

--- work_time_table_mobile/lib/streamed_dao_helpers/user_dependent_value.dart
+++ work_time_table_mobile/lib/streamed_dao_helpers/user_dependent_value.dart
@@ -7,3 +7,10 @@ class UserValue<T> extends UserDependentValue<T> {
 
   final T value;
 }
+
+T runUserDependentAction<T, U>(UserDependentValue<U> user,
+        T Function() noUserAction, T Function(U value) userAction) =>
+    switch (user) {
+      NoUserValue() => noUserAction(),
+      UserValue(value: var userValue) => userAction(userValue),
+    };
