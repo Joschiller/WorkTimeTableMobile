f06c630994939879aef11fa026bde2c74499709e
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-19 15:07:04 +0200

    feat(event_type): priority

--- work_time_table_mobile/lib/models/event_setting/event_type.dart
+++ work_time_table_mobile/lib/models/event_setting/event_type.dart
@@ -1,7 +1,13 @@
 enum EventType {
-  publicHoliday,
-  vacation,
-  sickDay,
-  dayOff,
-  businessTrip;
+  publicHoliday(priority: 0),
+  vacation(priority: 4),
+  sickDay(priority: 1),
+  dayOff(priority: 2),
+  businessTrip(priority: 3);
+
+  const EventType({required this.priority});
+
+  final int priority;
 }
+
+// TODO: the priority is used to select the event for a day -> lowest index wins if there is a duplicate for a certain day
