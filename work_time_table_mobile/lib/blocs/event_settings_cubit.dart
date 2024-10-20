4df700bb68fec16782f326567b7f071c40e6fc41
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-20 15:15:51 +0200

    feat: EventSettingsCubit

---
+++ work_time_table_mobile/lib/blocs/event_settings_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:work_time_table_mobile/models/event_setting/event_setting.dart';
+import 'package:work_time_table_mobile/services/event_setting_service.dart';
+import 'package:work_time_table_mobile/streamed_dao_helpers/context/context_dependent_cubit.dart';
+
+typedef EventSettingsCubitState = List<EventSetting>;
+
+class EventSettingsCubit
+    extends ContextDependentCubit<EventSettingsCubitState> {
+  EventSettingsCubit(this.eventSettingsService) : super() {
+    eventSettingsService.eventSettingDao.stream.listen(emit);
+  }
+
+  EventSettingService eventSettingsService;
+
+  Future<void> addEvent(EventSetting event) =>
+      eventSettingsService.addEvent(event);
+
+  Future<void> deleteEvent(int id, bool isConfirmed) =>
+      eventSettingsService.deleteEvent(id, isConfirmed);
+}
