ab43dbb2b7b8a6edef67cb894bfa43100d45831e
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-23 17:56:43 +0200

    feat: GlobalSettingService

---
+++ work_time_table_mobile/lib/services/global_setting_service.dart
@@ -0,0 +1,42 @@
+import 'package:work_time_table_mobile/app_error.dart';
+import 'package:work_time_table_mobile/daos/current_user_dao.dart';
+import 'package:work_time_table_mobile/daos/global_setting_dao.dart';
+import 'package:work_time_table_mobile/models/global_setting_key.dart';
+import 'package:work_time_table_mobile/models/settings_map.dart';
+import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_stream.dart';
+import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_value.dart';
+import 'package:work_time_table_mobile/stream_helpers/streamable_service.dart';
+
+final _stream = ContextDependentStream<SettingsMap>();
+
+class GlobalSettingService extends StreamableService {
+  GlobalSettingService(this._currentUserDao, this._globalSettingDao) {
+    _currentUserDao.stream.stream
+        .listen((selectedUser) => runContextDependentAction(
+              selectedUser,
+              () => _loadData(null),
+              (user) => _loadData(user.id),
+            ));
+    prepareListen(_globalSettingDao.stream, _stream);
+  }
+
+  final CurrentUserDao _currentUserDao;
+  final GlobalSettingDao _globalSettingDao;
+
+  Stream<ContextDependentValue<SettingsMap>> get globalSettingStream =>
+      _stream.stream;
+
+  Future<void> _loadData(int? userId) =>
+      _globalSettingDao.loadUserValues(userId);
+
+  Future<void> updateByKey(
+    GlobalSettingKey key,
+    String? value,
+  ) =>
+      runContextDependentAction(
+        _currentUserDao.stream.state,
+        () async => Future.error(AppError.service_noUserLoaded),
+        // TODO: Validation
+        (user) => _globalSettingDao.updateByUserIdAndKey(user.id, key, value),
+      );
+}
