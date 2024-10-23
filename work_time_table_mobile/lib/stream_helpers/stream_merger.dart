88790ab670a3461219c1ddad9a5c222dc11db40c
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-23 18:44:32 +0200

    refactor(stream_merger): improve variable names

--- work_time_table_mobile/lib/stream_helpers/stream_merger.dart
+++ work_time_table_mobile/lib/stream_helpers/stream_merger.dart
@@ -17,9 +17,9 @@ class MergedStreamValue<T> {
 Stream<MergedStreamValue> mergeStream(List<StreamIdentity> streams) {
   final controller = StreamController<MergedStreamValue>();
 
-  for (var identity in streams) {
-    identity.stream
-        .listen((data) => controller.add(MergedStreamValue(identity, data)));
+  for (var stream in streams) {
+    stream.stream
+        .listen((data) => controller.add(MergedStreamValue(stream, data)));
   }
 
   return controller.stream;
