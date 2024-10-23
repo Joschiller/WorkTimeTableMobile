26e20039c9f84f6cbc002d5e670f38f3341739e3
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-23 19:57:58 +0200

    refactor(TimeInputService): move day method to own service

---
+++ work_time_table_mobile/lib/services/day_value_service.dart
@@ -0,0 +1,93 @@
+import 'package:work_time_table_mobile/models/event_setting/event_setting.dart';
+import 'package:work_time_table_mobile/models/event_setting/event_type.dart';
+import 'package:work_time_table_mobile/models/value/day_mode.dart';
+import 'package:work_time_table_mobile/models/value/day_value.dart';
+import 'package:work_time_table_mobile/models/week_setting/day_of_week.dart';
+import 'package:work_time_table_mobile/models/week_setting/week_setting.dart';
+import 'package:work_time_table_mobile/services/event_service.dart';
+
+class DayValueService {
+  const DayValueService(this._eventService);
+
+  final EventService _eventService;
+
+  DayValue getInitialValueForDay(
+    DateTime date,
+    WeekSetting weekSetting,
+    List<EventSetting> eventSettings,
+  ) {
+    final daySetting =
+        weekSetting.weekDaySettings[DayOfWeek.fromDateTime(date)];
+
+    // if nonWorkDay -> nonWorkDay
+    if (daySetting == null) {
+      return DayValue(
+        date: date,
+        firstHalfMode: DayMode.nonWorkDay,
+        secondHalfMode: DayMode.nonWorkDay,
+        workTimeStart: 0,
+        workTimeEnd: 0,
+        breakDuration: 0,
+      );
+    }
+
+    // check if any event is set for part of the day
+    var modeForFirstHalfOfDay = DayMode.workDay;
+    var modeForSecondHalfOfDay = DayMode.workDay;
+
+    for (final eventType in EventType.values
+      ..sort((a, b) => a.priority - b.priority)) {
+      for (final event
+          in eventSettings.where((event) => event.eventType == eventType)) {
+        // check event range
+        final eventInfluence = _eventService.doesEventAffectDate(date, event);
+
+        if (eventInfluence.firstHalf &&
+            modeForFirstHalfOfDay == DayMode.workDay) {
+          modeForFirstHalfOfDay = DayMode.fromEventType(event.eventType);
+        }
+
+        if (eventInfluence.secondHalf &&
+            modeForSecondHalfOfDay == DayMode.workDay) {
+          modeForSecondHalfOfDay = DayMode.fromEventType(event.eventType);
+        }
+
+        if (modeForFirstHalfOfDay != DayMode.workDay &&
+            modeForSecondHalfOfDay != DayMode.workDay) {
+          break;
+        }
+      }
+
+      if (modeForFirstHalfOfDay != DayMode.workDay &&
+          modeForSecondHalfOfDay != DayMode.workDay) {
+        break;
+      }
+    }
+
+    if (modeForFirstHalfOfDay != DayMode.workDay &&
+        modeForSecondHalfOfDay != DayMode.workDay) {
+      // found a non working day
+      return DayValue(
+        date: date,
+        firstHalfMode: modeForFirstHalfOfDay,
+        secondHalfMode: modeForSecondHalfOfDay,
+        workTimeStart: 0,
+        workTimeEnd: 0,
+        breakDuration: 0,
+      );
+    }
+
+    // built up day value respecting default and core time if necessary
+    return DayValue(
+      date: date,
+      firstHalfMode: modeForFirstHalfOfDay,
+      secondHalfMode: modeForSecondHalfOfDay,
+      workTimeStart: daySetting.defaultWorkTimeStart ??
+          weekSetting.globalWeekDaySetting.defaultWorkTimeStart,
+      workTimeEnd: daySetting.defaultWorkTimeEnd ??
+          weekSetting.globalWeekDaySetting.defaultWorkTimeEnd,
+      breakDuration: daySetting.defaultBreakDuration ??
+          weekSetting.globalWeekDaySetting.defaultBreakDuration,
+    );
+  }
+}
--- work_time_table_mobile/lib/services/time_input_service.dart
+++ work_time_table_mobile/lib/services/time_input_service.dart
@@ -5,13 +5,12 @@ import 'package:work_time_table_mobile/daos/event_setting_dao.dart';
 import 'package:work_time_table_mobile/daos/week_setting_dao.dart';
 import 'package:work_time_table_mobile/daos/week_value_dao.dart';
 import 'package:work_time_table_mobile/models/event_setting/event_setting.dart';
-import 'package:work_time_table_mobile/models/event_setting/event_type.dart';
 import 'package:work_time_table_mobile/models/value/day_mode.dart';
 import 'package:work_time_table_mobile/models/value/day_value.dart';
 import 'package:work_time_table_mobile/models/value/week_value.dart';
 import 'package:work_time_table_mobile/models/week_setting/day_of_week.dart';
 import 'package:work_time_table_mobile/models/week_setting/week_setting.dart';
-import 'package:work_time_table_mobile/services/event_service.dart';
+import 'package:work_time_table_mobile/services/day_value_service.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_stream.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_value.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/list/context_dependent_list_stream.dart';
@@ -57,7 +56,7 @@ class TimeInputService extends StreamableService {
     this._eventSettingDao,
     this._dayValueDao,
     this._weekValueDao,
-    this._eventService,
+    this._dayValueService,
   ) {
     _currentUserDao.stream.stream
         .listen((selectedUser) => runContextDependentAction(
@@ -76,7 +75,7 @@ class TimeInputService extends StreamableService {
   final EventSettingDao _eventSettingDao;
   final DayValueDao _dayValueDao;
   final WeekValueDao _weekValueDao;
-  final EventService _eventService;
+  final DayValueService _dayValueService;
 
   Stream<ContextDependentValue<WeekSetting>> get weekSettingStream =>
       _weekSettingStream.stream;
@@ -139,7 +138,7 @@ class TimeInputService extends StreamableService {
           (values.dayValues
                   .where((day) => day.date == dateOfDay)
                   .firstOrNull) ??
-              _getInitialValueForDay(
+              _dayValueService.getInitialValueForDay(
                 dateOfDay,
                 values.weekSetting,
                 values.eventSettings,
@@ -166,86 +165,6 @@ class TimeInputService extends StreamableService {
               .reduce((a, b) => a + b) -
           (week?.targetTime ?? values.weekSetting.targetWorkTimePerWeek),
       weekClosed: week != null,
-    );
-  }
-
-  DayValue _getInitialValueForDay(
-    DateTime date,
-    WeekSetting weekSetting,
-    List<EventSetting> eventSettings,
-  ) {
-    final daySetting =
-        weekSetting.weekDaySettings[DayOfWeek.fromDateTime(date)];
-
-    // if nonWorkDay -> nonWorkDay
-    if (daySetting == null) {
-      return DayValue(
-        date: date,
-        firstHalfMode: DayMode.nonWorkDay,
-        secondHalfMode: DayMode.nonWorkDay,
-        workTimeStart: 0,
-        workTimeEnd: 0,
-        breakDuration: 0,
-      );
-    }
-
-    // check if any event is set for part of the day
-    var modeForFirstHalfOfDay = DayMode.workDay;
-    var modeForSecondHalfOfDay = DayMode.workDay;
-
-    for (final eventType in EventType.values
-      ..sort((a, b) => a.priority - b.priority)) {
-      for (final event
-          in eventSettings.where((event) => event.eventType == eventType)) {
-        // check event range
-        final eventInfluence = _eventService.doesEventAffectDate(date, event);
-
-        if (eventInfluence.firstHalf &&
-            modeForFirstHalfOfDay == DayMode.workDay) {
-          modeForFirstHalfOfDay = DayMode.fromEventType(event.eventType);
-        }
-
-        if (eventInfluence.secondHalf &&
-            modeForSecondHalfOfDay == DayMode.workDay) {
-          modeForSecondHalfOfDay = DayMode.fromEventType(event.eventType);
-        }
-
-        if (modeForFirstHalfOfDay != DayMode.workDay &&
-            modeForSecondHalfOfDay != DayMode.workDay) {
-          break;
-        }
-      }
-
-      if (modeForFirstHalfOfDay != DayMode.workDay &&
-          modeForSecondHalfOfDay != DayMode.workDay) {
-        break;
-      }
-    }
-
-    if (modeForFirstHalfOfDay != DayMode.workDay &&
-        modeForSecondHalfOfDay != DayMode.workDay) {
-      // found a non working day
-      return DayValue(
-        date: date,
-        firstHalfMode: modeForFirstHalfOfDay,
-        secondHalfMode: modeForSecondHalfOfDay,
-        workTimeStart: 0,
-        workTimeEnd: 0,
-        breakDuration: 0,
-      );
-    }
-
-    // built up day value respecting default and core time if necessary
-    return DayValue(
-      date: date,
-      firstHalfMode: modeForFirstHalfOfDay,
-      secondHalfMode: modeForSecondHalfOfDay,
-      workTimeStart: daySetting.defaultWorkTimeStart ??
-          weekSetting.globalWeekDaySetting.defaultWorkTimeStart,
-      workTimeEnd: daySetting.defaultWorkTimeEnd ??
-          weekSetting.globalWeekDaySetting.defaultWorkTimeEnd,
-      breakDuration: daySetting.defaultBreakDuration ??
-          weekSetting.globalWeekDaySetting.defaultBreakDuration,
     );
   }
 
