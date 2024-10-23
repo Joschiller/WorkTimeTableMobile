a138eb5041395331152da67eb74a73d7c59d780e
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-23 19:52:51 +0200

    refactor(TimeInputService): move event methods to own service

---
+++ work_time_table_mobile/lib/services/event_service.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:work_time_table_mobile/models/event_setting/day_based_repetition_rule.dart';
+import 'package:work_time_table_mobile/models/event_setting/event_setting.dart';
+import 'package:work_time_table_mobile/models/event_setting/month_based_repetition_rule.dart';
+import 'package:work_time_table_mobile/models/week_setting/day_of_week.dart';
+
+class EventService {
+  ({bool firstHalf, bool secondHalf}) doesEventAffectDate(
+      DateTime targetDate, EventSetting event) {
+    var firstHalf = false;
+    var secondHalf = false;
+    // check event base values
+    final eventRangeCheck = _isDateInRange(targetDate, (
+      start: event.startDate,
+      end: event.endDate,
+      startIsHalfDay: event.startIsHalfDay,
+      endIsHalfDay: event.endIsHalfDay,
+    ));
+    firstHalf = firstHalf || eventRangeCheck.firstHalf;
+    secondHalf = secondHalf || eventRangeCheck.secondHalf;
+
+    final eventDuration = DateTimeRange(
+      start: event.startDate,
+      end: event.endDate,
+    ).duration;
+
+    // check repetitions
+    for (final daybasedRepetition in event.dayBasedRepetitionRules) {
+      var currentStartDate = _getNextOccurenceOfDayBasedRepetition(
+        event.startDate,
+        daybasedRepetition,
+      );
+      while (!targetDate.isBefore(currentStartDate)) {
+        // check range
+        final eventRangeCheck = _isDateInRange(targetDate, (
+          start: currentStartDate,
+          end: currentStartDate.add(eventDuration),
+          startIsHalfDay: event.startIsHalfDay,
+          endIsHalfDay: event.endIsHalfDay,
+        ));
+        firstHalf = firstHalf || eventRangeCheck.firstHalf;
+        secondHalf = secondHalf || eventRangeCheck.secondHalf;
+
+        currentStartDate = _getNextOccurenceOfDayBasedRepetition(
+          currentStartDate,
+          daybasedRepetition,
+        );
+      }
+    }
+    for (final monthBasedRepetitions in event.monthBasedRepetitionRules) {
+      var currentStartDate = _getNextOccurenceOfMonthBasedRepetition(
+        event.startDate,
+        monthBasedRepetitions,
+      );
+      while (!targetDate.isBefore(currentStartDate)) {
+        // check range
+        final eventRangeCheck = _isDateInRange(targetDate, (
+          start: currentStartDate,
+          end: currentStartDate.add(eventDuration),
+          startIsHalfDay: event.startIsHalfDay,
+          endIsHalfDay: event.endIsHalfDay,
+        ));
+        firstHalf = firstHalf || eventRangeCheck.firstHalf;
+        secondHalf = secondHalf || eventRangeCheck.secondHalf;
+
+        currentStartDate = _getNextOccurenceOfMonthBasedRepetition(
+          currentStartDate,
+          monthBasedRepetitions,
+        );
+      }
+    }
+
+    return (firstHalf: firstHalf, secondHalf: secondHalf);
+  }
+
+  DateTime _getNextOccurenceOfDayBasedRepetition(
+    DateTime currentDate,
+    DayBasedRepetitionRule repetition,
+  ) =>
+      currentDate.add(Duration(
+        days: repetition.repeatAfterDays,
+      ));
+
+  DateTime _getNextOccurenceOfMonthBasedRepetition(
+    DateTime currentDate,
+    MonthBasedRepetitionRule repetition,
+  ) {
+    final targetMonth = DateTime(
+      currentDate.year,
+      currentDate.month + repetition.repeatAfterMonths,
+    );
+    final countOfDaysInMonth = DateTimeRange(
+      start: targetMonth,
+      end: DateTime(targetMonth.year, targetMonth.month + 1),
+    ).duration.inDays;
+
+    final weekIndex = repetition.weekIndex;
+
+    if (weekIndex == null) {
+      return DateTime(
+        targetMonth.year,
+        targetMonth.month,
+        repetition.countFromEnd
+            ? countOfDaysInMonth - repetition.dayIndex
+            : repetition.dayIndex + 1,
+      );
+    } else {
+      final instancesOfDayOfWeek = <DateTime>[];
+      for (var i = 0; i < countOfDaysInMonth; i++) {
+        final dayToTest = targetMonth.add(Duration(days: i));
+        if (DayOfWeek.fromDateTime(dayToTest) ==
+                DayOfWeek.values[repetition.dayIndex]
+            // check hours for some special cases (e.g. searching for sundays in 10/2024)
+            &&
+            dayToTest.hour == 0) {
+          instancesOfDayOfWeek.add(dayToTest);
+        }
+      }
+      return instancesOfDayOfWeek[repetition.countFromEnd
+          ? instancesOfDayOfWeek.length - weekIndex - 1
+          : weekIndex];
+    }
+  }
+
+  ({bool firstHalf, bool secondHalf}) _isDateInRange(
+      DateTime targetDate,
+      ({
+        DateTime start,
+        DateTime end,
+        bool startIsHalfDay,
+        bool endIsHalfDay,
+      }) range) {
+    if (!targetDate.isBefore(range.start) && !targetDate.isAfter(range.end)) {
+      final firstHalf =
+          !range.startIsHalfDay || range.start.isBefore(targetDate);
+      final secondHalf = !range.endIsHalfDay || range.end.isAfter(targetDate);
+      return (firstHalf: firstHalf, secondHalf: secondHalf);
+    }
+    return (firstHalf: false, secondHalf: false);
+  }
+}
--- work_time_table_mobile/lib/services/time_input_service.dart
+++ work_time_table_mobile/lib/services/time_input_service.dart
@@ -1,19 +1,17 @@
-import 'package:flutter/material.dart';
 import 'package:work_time_table_mobile/app_error.dart';
 import 'package:work_time_table_mobile/daos/current_user_dao.dart';
 import 'package:work_time_table_mobile/daos/day_value_dao.dart';
 import 'package:work_time_table_mobile/daos/event_setting_dao.dart';
 import 'package:work_time_table_mobile/daos/week_setting_dao.dart';
 import 'package:work_time_table_mobile/daos/week_value_dao.dart';
-import 'package:work_time_table_mobile/models/event_setting/day_based_repetition_rule.dart';
 import 'package:work_time_table_mobile/models/event_setting/event_setting.dart';
 import 'package:work_time_table_mobile/models/event_setting/event_type.dart';
-import 'package:work_time_table_mobile/models/event_setting/month_based_repetition_rule.dart';
 import 'package:work_time_table_mobile/models/value/day_mode.dart';
 import 'package:work_time_table_mobile/models/value/day_value.dart';
 import 'package:work_time_table_mobile/models/value/week_value.dart';
 import 'package:work_time_table_mobile/models/week_setting/day_of_week.dart';
 import 'package:work_time_table_mobile/models/week_setting/week_setting.dart';
+import 'package:work_time_table_mobile/services/event_service.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_stream.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_value.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/list/context_dependent_list_stream.dart';
@@ -59,6 +57,7 @@ class TimeInputService extends StreamableService {
     this._eventSettingDao,
     this._dayValueDao,
     this._weekValueDao,
+    this._eventService,
   ) {
     _currentUserDao.stream.stream
         .listen((selectedUser) => runContextDependentAction(
@@ -77,6 +76,7 @@ class TimeInputService extends StreamableService {
   final EventSettingDao _eventSettingDao;
   final DayValueDao _dayValueDao;
   final WeekValueDao _weekValueDao;
+  final EventService _eventService;
 
   Stream<ContextDependentValue<WeekSetting>> get weekSettingStream =>
       _weekSettingStream.stream;
@@ -198,7 +198,7 @@ class TimeInputService extends StreamableService {
       for (final event
           in eventSettings.where((event) => event.eventType == eventType)) {
         // check event range
-        final eventInfluence = _doesEventAffectDate(date, event);
+        final eventInfluence = _eventService.doesEventAffectDate(date, event);
 
         if (eventInfluence.firstHalf &&
             modeForFirstHalfOfDay == DayMode.workDay) {
@@ -247,140 +247,6 @@ class TimeInputService extends StreamableService {
       breakDuration: daySetting.defaultBreakDuration ??
           weekSetting.globalWeekDaySetting.defaultBreakDuration,
     );
-  }
-
-  ({bool firstHalf, bool secondHalf}) _doesEventAffectDate(
-      DateTime targetDate, EventSetting event) {
-    var firstHalf = false;
-    var secondHalf = false;
-    // check event base values
-    final eventRangeCheck = _isDateInRange(targetDate, (
-      start: event.startDate,
-      end: event.endDate,
-      startIsHalfDay: event.startIsHalfDay,
-      endIsHalfDay: event.endIsHalfDay,
-    ));
-    firstHalf = firstHalf || eventRangeCheck.firstHalf;
-    secondHalf = secondHalf || eventRangeCheck.secondHalf;
-
-    final eventDuration = DateTimeRange(
-      start: event.startDate,
-      end: event.endDate,
-    ).duration;
-
-    // check repetitions
-    for (final daybasedRepetition in event.dayBasedRepetitionRules) {
-      var currentStartDate = _getNextOccurenceOfDayBasedRepetition(
-        event.startDate,
-        daybasedRepetition,
-      );
-      while (!targetDate.isBefore(currentStartDate)) {
-        // check range
-        final eventRangeCheck = _isDateInRange(targetDate, (
-          start: currentStartDate,
-          end: currentStartDate.add(eventDuration),
-          startIsHalfDay: event.startIsHalfDay,
-          endIsHalfDay: event.endIsHalfDay,
-        ));
-        firstHalf = firstHalf || eventRangeCheck.firstHalf;
-        secondHalf = secondHalf || eventRangeCheck.secondHalf;
-
-        currentStartDate = _getNextOccurenceOfDayBasedRepetition(
-          currentStartDate,
-          daybasedRepetition,
-        );
-      }
-    }
-    for (final monthBasedRepetitions in event.monthBasedRepetitionRules) {
-      var currentStartDate = _getNextOccurenceOfMonthBasedRepetition(
-        event.startDate,
-        monthBasedRepetitions,
-      );
-      while (!targetDate.isBefore(currentStartDate)) {
-        // check range
-        final eventRangeCheck = _isDateInRange(targetDate, (
-          start: currentStartDate,
-          end: currentStartDate.add(eventDuration),
-          startIsHalfDay: event.startIsHalfDay,
-          endIsHalfDay: event.endIsHalfDay,
-        ));
-        firstHalf = firstHalf || eventRangeCheck.firstHalf;
-        secondHalf = secondHalf || eventRangeCheck.secondHalf;
-
-        currentStartDate = _getNextOccurenceOfMonthBasedRepetition(
-          currentStartDate,
-          monthBasedRepetitions,
-        );
-      }
-    }
-
-    return (firstHalf: firstHalf, secondHalf: secondHalf);
-  }
-
-  DateTime _getNextOccurenceOfDayBasedRepetition(
-    DateTime currentDate,
-    DayBasedRepetitionRule repetition,
-  ) =>
-      currentDate.add(Duration(
-        days: repetition.repeatAfterDays,
-      ));
-
-  DateTime _getNextOccurenceOfMonthBasedRepetition(
-    DateTime currentDate,
-    MonthBasedRepetitionRule repetition,
-  ) {
-    final targetMonth = DateTime(
-      currentDate.year,
-      currentDate.month + repetition.repeatAfterMonths,
-    );
-    final countOfDaysInMonth = DateTimeRange(
-      start: targetMonth,
-      end: DateTime(targetMonth.year, targetMonth.month + 1),
-    ).duration.inDays;
-
-    final weekIndex = repetition.weekIndex;
-
-    if (weekIndex == null) {
-      return DateTime(
-        targetMonth.year,
-        targetMonth.month,
-        repetition.countFromEnd
-            ? countOfDaysInMonth - repetition.dayIndex
-            : repetition.dayIndex + 1,
-      );
-    } else {
-      final instancesOfDayOfWeek = <DateTime>[];
-      for (var i = 0; i < countOfDaysInMonth; i++) {
-        final dayToTest = targetMonth.add(Duration(days: i));
-        if (DayOfWeek.fromDateTime(dayToTest) ==
-                DayOfWeek.values[repetition.dayIndex]
-            // check hours for some special cases (e.g. searching for sundays in 10/2024)
-            &&
-            dayToTest.hour == 0) {
-          instancesOfDayOfWeek.add(dayToTest);
-        }
-      }
-      return instancesOfDayOfWeek[repetition.countFromEnd
-          ? instancesOfDayOfWeek.length - weekIndex - 1
-          : weekIndex];
-    }
-  }
-
-  ({bool firstHalf, bool secondHalf}) _isDateInRange(
-      DateTime targetDate,
-      ({
-        DateTime start,
-        DateTime end,
-        bool startIsHalfDay,
-        bool endIsHalfDay,
-      }) range) {
-    if (!targetDate.isBefore(range.start) && !targetDate.isAfter(range.end)) {
-      final firstHalf =
-          !range.startIsHalfDay || range.start.isBefore(targetDate);
-      final secondHalf = !range.endIsHalfDay || range.end.isAfter(targetDate);
-      return (firstHalf: firstHalf, secondHalf: secondHalf);
-    }
-    return (firstHalf: false, secondHalf: false);
   }
 
   static DateTime getStartDateOfWeek(DateTime date) =>
