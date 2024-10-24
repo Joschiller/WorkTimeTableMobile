a77379ce8a3d6dbd66f3f30037ef9a65af3d7e32
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-24 17:28:29 +0200

    refactor(global_setting_service): use UserService  instead of dao

--- work_time_table_mobile/lib/services/global_setting_service.dart
+++ work_time_table_mobile/lib/services/global_setting_service.dart
@@ -1,8 +1,8 @@
 import 'package:work_time_table_mobile/app_error.dart';
-import 'package:work_time_table_mobile/daos/current_user_dao.dart';
 import 'package:work_time_table_mobile/daos/global_setting_dao.dart';
 import 'package:work_time_table_mobile/models/global_setting_key.dart';
 import 'package:work_time_table_mobile/models/settings_map.dart';
+import 'package:work_time_table_mobile/services/user_service.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_stream.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_value.dart';
 import 'package:work_time_table_mobile/stream_helpers/streamable_service.dart';
@@ -10,8 +10,8 @@ import 'package:work_time_table_mobile/stream_helpers/streamable_service.dart';
 final _stream = ContextDependentStream<SettingsMap>();
 
 class GlobalSettingService extends StreamableService {
-  GlobalSettingService(this._currentUserDao, this._globalSettingDao) {
-    _currentUserDao.stream.stream
+  GlobalSettingService(this._userService, this._globalSettingDao) {
+    _userService.currentUserStream.stream
         .listen((selectedUser) => runContextDependentAction(
               selectedUser,
               () => _loadData(null),
@@ -20,7 +20,7 @@ class GlobalSettingService extends StreamableService {
     prepareListen(_globalSettingDao.stream, _stream);
   }
 
-  final CurrentUserDao _currentUserDao;
+  final UserService _userService;
   final GlobalSettingDao _globalSettingDao;
 
   ContextDependentStream<SettingsMap> get globalSettingStream => _stream;
@@ -33,7 +33,7 @@ class GlobalSettingService extends StreamableService {
     String? value,
   ) =>
       runContextDependentAction(
-        _currentUserDao.stream.state,
+        _userService.currentUserStream.state,
         () async => Future.error(AppError.service_noUserLoaded),
         // TODO: Validation
         (user) => _globalSettingDao.updateByUserIdAndKey(user.id, key, value),
