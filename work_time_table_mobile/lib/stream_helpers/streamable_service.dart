58de211f9f55d1c5b40794b6590ef34640912016
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-24 17:14:26 +0200

    refactor(streamable_service): improve variable names

--- work_time_table_mobile/lib/stream_helpers/streamable_service.dart
+++ work_time_table_mobile/lib/stream_helpers/streamable_service.dart
@@ -2,22 +2,22 @@ import 'package:work_time_table_mobile/stream_helpers/cached_stream.dart';
 
 class StreamableService {
   void prepareListen<T>(
-    CachedStream<T> daoStream,
-    CachedStream<T> serviceStream,
+    CachedStream<T> sourceStream,
+    CachedStream<T> targetStream,
   ) {
-    serviceStream.emitReload(daoStream.state);
-    daoStream.stream.listen(serviceStream.emitReload);
+    targetStream.emitReload(sourceStream.state);
+    sourceStream.stream.listen(targetStream.emitReload);
   }
 
   void prepareComplexListen<T>(
-    List<CachedStream> daoStreams,
+    List<CachedStream> sourceStreams,
     T Function() generateNext,
-    CachedStream<T> serviceStream,
+    CachedStream<T> targetStream,
   ) {
-    serviceStream.emitReload(generateNext());
-    for (var daoStream in daoStreams) {
-      daoStream.stream
-          .listen((data) => serviceStream.emitReload(generateNext()));
+    targetStream.emitReload(generateNext());
+    for (var sourceStream in sourceStreams) {
+      sourceStream.stream
+          .listen((data) => targetStream.emitReload(generateNext()));
     }
   }
 }
