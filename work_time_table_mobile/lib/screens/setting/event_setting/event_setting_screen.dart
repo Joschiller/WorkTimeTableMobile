d4d179fe3f39ee147dcb961b45e0674c25d9108d
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-25 15:07:48 +0200

    feat: prepare screens

---
+++ work_time_table_mobile/lib/screens/setting/event_setting/event_setting_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class EventSettingScreen extends StatelessWidget {
+  const EventSettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ work_time_table_mobile/lib/screens/setting/week_setting/week_setting_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class WeekSettingScreen extends StatelessWidget {
+  const WeekSettingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
