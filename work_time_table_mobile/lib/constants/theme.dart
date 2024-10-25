dcadb03868944742a7f5104ae9439560bea1f814
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-25 15:10:42 +0200

    feat: initial theme

---
+++ work_time_table_mobile/lib/constants/theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+// TODO: customize theme
+
+final colorScheme = ColorScheme.fromSeed(
+  seedColor: const Color(0xffFBB834),
+  secondary: const Color(0xff05B87C),
+  tertiary: const Color(0xff019567),
+  error: const Color(0xffFF5449),
+  surface: const Color(0xfffff8f0),
+  surfaceDim: const Color(0xffFFF7E4),
+);
+
+const buttonTheme = ButtonThemeData(
+  buttonColor: Color(0xffFFE8A6),
+);
+
+const cardTheme = CardTheme(
+  color: Colors.white,
+);
