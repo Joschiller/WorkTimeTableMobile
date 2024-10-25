9ebcc0661b152e736b167302034d87c1c97fca5c
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-25 15:10:51 +0200

    feat: wire dependencies and routes

--- work_time_table_mobile/lib/main.dart
+++ work_time_table_mobile/lib/main.dart
@@ -1,69 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:work_time_table_mobile/blocs/current_user_cubit.dart';
+import 'package:work_time_table_mobile/blocs/error_cubit.dart';
+import 'package:work_time_table_mobile/blocs/event_setting_cubit.dart';
+import 'package:work_time_table_mobile/blocs/global_setting_cubit.dart';
+import 'package:work_time_table_mobile/blocs/time_input_cubit.dart';
+import 'package:work_time_table_mobile/blocs/user_cubit.dart';
+import 'package:work_time_table_mobile/blocs/week_setting_cubit.dart';
+import 'package:work_time_table_mobile/constants/app_name.dart';
+import 'package:work_time_table_mobile/constants/routes.dart';
+import 'package:work_time_table_mobile/constants/theme.dart';
+import 'package:work_time_table_mobile/daos/current_user_dao.dart';
+import 'package:work_time_table_mobile/daos/day_value_dao.dart';
+import 'package:work_time_table_mobile/daos/event_setting_dao.dart';
+import 'package:work_time_table_mobile/daos/global_setting_dao.dart';
+import 'package:work_time_table_mobile/daos/user_dao.dart';
+import 'package:work_time_table_mobile/daos/week_setting_dao.dart';
+import 'package:work_time_table_mobile/daos/week_value_dao.dart';
+import 'package:work_time_table_mobile/services/day_value_service.dart';
+import 'package:work_time_table_mobile/services/event_service.dart';
+import 'package:work_time_table_mobile/services/event_setting_service.dart';
+import 'package:work_time_table_mobile/services/global_setting_service.dart';
+import 'package:work_time_table_mobile/services/time_input_service.dart';
+import 'package:work_time_table_mobile/services/user_service.dart';
+import 'package:work_time_table_mobile/services/week_setting_service.dart';
+import 'package:work_time_table_mobile/services/week_value_service.dart';
 
 void main() {
-  runApp(const MyApp());
+  final userService = UserService(const UserDao(), CurrentUserDao());
+  final globalSettingService =
+      GlobalSettingService(userService, const GlobalSettingDao());
+  final weekSettingService =
+      WeekSettingService(userService, const WeekSettingDao());
+  final eventSettingService =
+      EventSettingService(userService, const EventSettingDao());
+  final timeInputService = TimeInputService(
+    userService,
+    weekSettingService,
+    eventSettingService,
+    const DayValueDao(),
+    const WeekValueDao(),
+    const WeekValueService(DayValueService(EventService())),
+  );
+
+  runApp(MyApp(
+    userService: userService,
+    globalSettingService: globalSettingService,
+    weekSettingService: weekSettingService,
+    eventSettingService: eventSettingService,
+    timeInputService: timeInputService,
+  ));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({
+    super.key,
+    required this.userService,
+    required this.globalSettingService,
+    required this.weekSettingService,
+    required this.eventSettingService,
+    required this.timeInputService,
+  });
+
+  final UserService userService;
+  final GlobalSettingService globalSettingService;
+  final WeekSettingService weekSettingService;
+  final EventSettingService eventSettingService;
+  final TimeInputService timeInputService;
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+  Widget build(BuildContext context) => MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => ErrorCubit()),
+          BlocProvider(create: (_) => UserCubit(userService)),
+          BlocProvider(create: (_) => CurrentUserCubit(userService)),
+          BlocProvider(create: (_) => GlobalSettingCubit(globalSettingService)),
+          BlocProvider(create: (_) => WeekSettingCubit(weekSettingService)),
+          BlocProvider(create: (_) => EventSettingCubit(eventSettingService)),
+          BlocProvider(
+              create: (_) => TimeInputCubit(
+                    userService,
+                    weekSettingService,
+                    eventSettingService,
+                    timeInputService,
+                  )),
+        ],
+        child: MaterialApp.router(
+          title: appName,
+          theme: ThemeData(
+            colorScheme: colorScheme,
+            useMaterial3: true,
+            buttonTheme: buttonTheme,
+            cardTheme: cardTheme,
+          ),
+          routerConfig: GoRouter(routes: $appRoutes),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+      );
 }
