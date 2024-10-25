ba38c90793a379c1cbf2ae40ff3f3481b6f1e2b1
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-25 15:01:56 +0200

    refactor(event_setting_cubit): rename variable names

--- work_time_table_mobile/lib/blocs/event_setting_cubit.dart
+++ work_time_table_mobile/lib/blocs/event_setting_cubit.dart
@@ -3,15 +3,15 @@ import 'package:work_time_table_mobile/services/event_setting_service.dart';
 import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_cubit.dart';
 
 class EventSettingCubit extends ContextDependentCubit<List<EventSetting>> {
-  EventSettingCubit(this._eventSettingsService) : super() {
-    _eventSettingsService.eventSettingStream.stream.listen(emit);
+  EventSettingCubit(this._eventSettingService) : super() {
+    _eventSettingService.eventSettingStream.stream.listen(emit);
   }
 
-  final EventSettingService _eventSettingsService;
+  final EventSettingService _eventSettingService;
 
   Future<void> addEvent(EventSetting event) =>
-      _eventSettingsService.addEvent(event);
+      _eventSettingService.addEvent(event);
 
   Future<void> deleteEvent(int id, bool isConfirmed) =>
-      _eventSettingsService.deleteEvent(id, isConfirmed);
+      _eventSettingService.deleteEvent(id, isConfirmed);
 }
