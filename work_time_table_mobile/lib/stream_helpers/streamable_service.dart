ce86b2751acf1bb29e2973a8278ab53c393f3c04
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-27 13:56:57 +0100

    feat(streamable_service): return stream subscriptions

--- work_time_table_mobile/lib/stream_helpers/streamable_service.dart
+++ work_time_table_mobile/lib/stream_helpers/streamable_service.dart
@@ -1,23 +1,25 @@
+import 'dart:async';
+
 import 'package:work_time_table_mobile/stream_helpers/cached_stream.dart';
 
 class StreamableService {
-  void prepareListen<T>(
+  StreamSubscription prepareListen<T>(
     CachedStream<T> sourceStream,
     CachedStream<T> targetStream,
   ) {
     targetStream.emitReload(sourceStream.state);
-    sourceStream.stream.listen(targetStream.emitReload);
+    return sourceStream.stream.listen(targetStream.emitReload);
   }
 
-  void prepareComplexListen<T>(
+  List<StreamSubscription> prepareComplexListen<T>(
     List<CachedStream> sourceStreams,
     T Function() generateNext,
     CachedStream<T> targetStream,
   ) {
     targetStream.emitReload(generateNext());
-    for (var sourceStream in sourceStreams) {
-      sourceStream.stream
-          .listen((data) => targetStream.emitReload(generateNext()));
-    }
+    return sourceStreams
+        .map((sourceStream) => sourceStream.stream
+            .listen((data) => targetStream.emitReload(generateNext())))
+        .toList();
   }
 }
