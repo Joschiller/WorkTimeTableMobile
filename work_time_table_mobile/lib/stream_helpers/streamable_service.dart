f45a6dff695782a1d70ad3428350b5d909117775
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-27 14:04:46 +0100

    feat(streamable_service): manage subscriptions

--- work_time_table_mobile/lib/stream_helpers/streamable_service.dart
+++ work_time_table_mobile/lib/stream_helpers/streamable_service.dart
@@ -3,23 +3,34 @@ import 'dart:async';
 import 'package:work_time_table_mobile/stream_helpers/cached_stream.dart';
 
 class StreamableService {
-  StreamSubscription prepareListen<T>(
+  final _subscriptions = <StreamSubscription>[];
+
+  void registerSubscription(StreamSubscription subscription) =>
+      _subscriptions.add(subscription);
+
+  void prepareListen<T>(
     CachedStream<T> sourceStream,
     CachedStream<T> targetStream,
   ) {
     targetStream.emitReload(sourceStream.state);
-    return sourceStream.stream.listen(targetStream.emitReload);
+    _subscriptions.add(sourceStream.stream.listen(targetStream.emitReload));
   }
 
-  List<StreamSubscription> prepareComplexListen<T>(
+  void prepareComplexListen<T>(
     List<CachedStream> sourceStreams,
     T Function() generateNext,
     CachedStream<T> targetStream,
   ) {
     targetStream.emitReload(generateNext());
-    return sourceStreams
+    _subscriptions.addAll(sourceStreams
         .map((sourceStream) => sourceStream.stream
             .listen((data) => targetStream.emitReload(generateNext())))
-        .toList();
+        .toList());
+  }
+
+  void close() {
+    for (var s in _subscriptions) {
+      s.cancel();
+    }
   }
 }
