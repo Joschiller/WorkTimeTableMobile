dad04c2fa1e56b728eb82c1d201218c56432b7e3
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-10-27 10:49:53 +0100

    feat: editable_list

---
+++ work_time_table_mobile/lib/components/editable_list.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:work_time_table_mobile/components/managable_list.dart';
+import 'package:work_time_table_mobile/components/page_template.dart';
+import 'package:work_time_table_mobile/stream_helpers/identifiable.dart';
+
+class EditableList<T extends Identifiable> extends StatefulWidget {
+  const EditableList({
+    super.key,
+    required this.title,
+    required this.items,
+    required this.templateItem,
+    required this.buildItem,
+    required this.onAdd,
+    required this.onRemove,
+    this.onTapItem,
+  });
+
+  final String title;
+
+  final List<T> items;
+  final T templateItem;
+
+  final Widget Function(T item, bool selected) buildItem;
+
+  final void Function() onAdd;
+  final void Function(List<T> items) onRemove;
+
+  final void Function(int index)? onTapItem;
+
+  @override
+  State<EditableList<T>> createState() => _EditableListState<T>();
+}
+
+class _EditableListState<T extends Identifiable>
+    extends State<EditableList<T>> {
+  final _selectedItems = <dynamic>[];
+
+  void _toggleSelection(int index) => setState(() {
+        final item = widget.items[index].identity;
+        if (_selectedItems.contains(item)) {
+          _selectedItems.remove(item);
+        } else {
+          _selectedItems.add(item);
+        }
+      });
+
+  @override
+  Widget build(BuildContext context) => PageTemplate(
+        title: widget.title,
+        content: ManagableList(
+          items: widget.items,
+          templateItem: widget.templateItem,
+          buildItem: (item) => widget.buildItem(
+            item,
+            _selectedItems.contains(item.identity),
+          ),
+          onTapItem:
+              _selectedItems.isEmpty ? widget.onTapItem : _toggleSelection,
+          onLongPressItem: _toggleSelection,
+        ),
+        floatingButton: _selectedItems.isEmpty
+            ? (
+                icon: Icon(
+                  Icons.add,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+                onPressed: widget.onAdd,
+              )
+            : (
+                icon: Icon(
+                  Icons.delete,
+                  color: Theme.of(context).colorScheme.error,
+                ),
+                onPressed: () => widget.onRemove(widget.items
+                    .where((item) => _selectedItems.contains(item.identity))
+                    .toList()),
+              ),
+      );
+}
