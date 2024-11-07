495668f9532d3370bf9dbd23ae3fc0cf472e008d
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-11-07 17:04:54 +0100

    feat: Validator

---
+++ work_time_table_mobile/lib/validator.dart
@@ -0,0 +1,48 @@
+import 'package:work_time_table_mobile/app_error.dart';
+
+class Validator {
+  Validator(this.validations);
+
+  final List<AppError? Function()> validations;
+
+  AppError? validate() {
+    for (final v in validations) {
+      final err = v();
+      if (err != null) {
+        return err;
+      }
+    }
+    return null;
+  }
+
+  List<AppError> validateAll() =>
+      validations.map((v) => v()).whereType<AppError>().toList();
+}
+
+class AsyncValidator {
+  AsyncValidator(this.validations);
+
+  final List<Future<AppError?> Function()> validations;
+
+  Future<AppError?> validate() async {
+    for (final v in validations) {
+      final err = await v();
+      if (err != null) {
+        return err;
+      }
+    }
+
+    return null;
+  }
+
+  Future<List<AppError>> validateAll() async {
+    final errors = <AppError>[];
+    for (final v in validations) {
+      final err = await v();
+      if (err != null) {
+        errors.add(err);
+      }
+    }
+    return errors;
+  }
+}
