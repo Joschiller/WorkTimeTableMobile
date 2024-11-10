20c0cb5e046cd141c97e4d3ac996ee47789c2de4
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-11-10 16:53:37 +0100

    feat: TimeInputButton

---
+++ work_time_table_mobile/lib/components/time_input/time_input_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:work_time_table_mobile/screens/setting/week_setting/week_setting_screen.dart';
+
+class TimeInputButton extends StatelessWidget {
+  const TimeInputButton({
+    super.key,
+    this.initialValue,
+    this.min,
+    this.max,
+    required this.onChange,
+  });
+
+  final TimeOfDay? initialValue;
+  final TimeOfDay? min;
+  final TimeOfDay? max;
+  final void Function(TimeOfDay value) onChange;
+
+  @override
+  Widget build(BuildContext context) => ElevatedButton(
+        onPressed: () => showTimePicker(
+          context: context,
+          initialTime: initialValue ?? const TimeOfDay(hour: 12, minute: 0),
+        ).then((value) {
+          if (value != null) {
+            onChange(min != null && value.toInt() < min!.toInt()
+                ? min!
+                : max != null && value.toInt() > max!.toInt()
+                    ? max!
+                    : value);
+          }
+        }),
+        child: Text(initialValue?.format(context) ?? 'Select Time'),
+      );
+}
