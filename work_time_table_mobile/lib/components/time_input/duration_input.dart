dbb45632cc6efda853ad0f9becdd9f81f26d0120
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-11-15 19:14:43 +0100

    feat: DurationInput

---
+++ work_time_table_mobile/lib/components/time_input/duration_input.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:numberpicker/numberpicker.dart';
+
+class DurationInput extends StatelessWidget {
+  const DurationInput({
+    super.key,
+    int? initialValue,
+    int? min,
+    int? max,
+    required this.onChange,
+  })  : initialValue = initialValue ?? 0,
+        min = min ?? 0,
+        max = max ?? 99 * 60;
+
+  final int initialValue;
+  final int min;
+  final int max;
+  final void Function(int value) onChange;
+
+  void _onChangeInBounds(int value) => onChange(value < min
+      ? min
+      : value > max
+          ? max
+          : value);
+
+  @override
+  Widget build(BuildContext context) => Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          NumberPicker(
+            value: initialValue ~/ 60,
+            minValue: (min / 60).floor(),
+            maxValue: (max / 60).ceil(),
+            onChanged: (value) =>
+                _onChangeInBounds((value * 60) + (initialValue % 60)),
+            zeroPad: true,
+          ),
+          NumberPicker(
+            value: initialValue % 60,
+            minValue: 0,
+            maxValue: 59,
+            onChanged: (value) =>
+                _onChangeInBounds(((initialValue ~/ 60) * 60) + value),
+            zeroPad: true,
+          ),
+        ],
+      );
+}
