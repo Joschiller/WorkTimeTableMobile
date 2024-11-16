51994a4cc739050994bfe51c69195d7b1b209b78
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-11-16 15:57:35 +0100

    refactor(model): remove global default settings

--- work_time_table_mobile/lib/models/week_setting/week_day_setting.dart
+++ work_time_table_mobile/lib/models/week_setting/week_day_setting.dart
@@ -3,21 +3,23 @@ import 'package:work_time_table_mobile/models/week_setting/day_of_week.dart';
 class WeekDaySetting {
   final DayOfWeek dayOfWeek;
 
-  final int? defaultWorkTimeStart;
-  final int? defaultWorkTimeEnd;
-  final int? mandatoryWorkTimeStart;
-  final int? mandatoryWorkTimeEnd;
-  final int? defaultBreakDuration;
-
   final int timeEquivalent;
+
+  final int mandatoryWorkTimeStart;
+  final int mandatoryWorkTimeEnd;
+
+  final int defaultWorkTimeStart;
+  final int defaultWorkTimeEnd;
+
+  final int defaultBreakDuration;
 
   WeekDaySetting({
     required this.dayOfWeek,
-    required this.defaultWorkTimeStart,
-    required this.defaultWorkTimeEnd,
+    required this.timeEquivalent,
     required this.mandatoryWorkTimeStart,
     required this.mandatoryWorkTimeEnd,
+    required this.defaultWorkTimeStart,
+    required this.defaultWorkTimeEnd,
     required this.defaultBreakDuration,
-    required this.timeEquivalent,
   });
 }
