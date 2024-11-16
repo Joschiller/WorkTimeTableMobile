bce84182dead52c8fcaf6b8bf655a31a102de2f9
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-11-16 15:04:15 +0100

    feat(time_span_input): min and max bounds

--- work_time_table_mobile/lib/components/time_input/time_span_input.dart
+++ work_time_table_mobile/lib/components/time_input/time_span_input.dart
@@ -1,14 +1,28 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:work_time_table_mobile/components/time_input/time_input_button.dart';
+import 'package:work_time_table_mobile/utils.dart';
 
 class TimeSpanInput extends StatelessWidget {
   const TimeSpanInput({
     super.key,
     required this.initialValue,
+    TimeOfDay? startMin,
+    TimeOfDay? startMax,
+    TimeOfDay? endMin,
+    TimeOfDay? endMax,
     required this.onChange,
-  });
+  })  : startMin = startMin ?? const TimeOfDay(hour: 0, minute: 0),
+        startMax = startMax ?? const TimeOfDay(hour: 23, minute: 59),
+        endMin = endMin ?? const TimeOfDay(hour: 0, minute: 0),
+        endMax = endMax ?? const TimeOfDay(hour: 23, minute: 59);
 
   final ({TimeOfDay? start, TimeOfDay? end}) initialValue;
+  final TimeOfDay startMin;
+  final TimeOfDay startMax;
+  final TimeOfDay endMin;
+  final TimeOfDay endMax;
   final void Function(TimeOfDay start, TimeOfDay end) onChange;
 
   @override
@@ -27,7 +41,11 @@ class TimeSpanInput extends StatelessWidget {
               ),
               TimeInputButton(
                 initialValue: initialValue.start,
-                max: initialValue.end,
+                min: startMin,
+                max: initialValue.end != null
+                    ? min(startMax.toInt(), initialValue.end!.toInt())
+                        .toTimeOfDay()
+                    : startMax,
                 onChange: (value) => onChange(value, initialValue.end ?? value),
               ),
             ],
@@ -44,7 +62,11 @@ class TimeSpanInput extends StatelessWidget {
               ),
               TimeInputButton(
                 initialValue: initialValue.end,
-                min: initialValue.start,
+                min: initialValue.start != null
+                    ? max(endMin.toInt(), initialValue.start!.toInt())
+                        .toTimeOfDay()
+                    : endMin,
+                max: endMax,
                 onChange: (value) =>
                     onChange(initialValue.start ?? value, value),
               ),
