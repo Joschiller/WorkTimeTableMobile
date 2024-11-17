85badf920456b3efa01920927b368e09c68649bf
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-11-17 16:29:29 +0100

    refactor(headers): use HeaderRow

--- work_time_table_mobile/lib/components/time_input/global_week_day_setting_input_header.dart
+++ work_time_table_mobile/lib/components/time_input/global_week_day_setting_input_header.dart
@@ -1,25 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:work_time_table_mobile/components/time_input/header_row.dart';
 
 class GlobalWeekDaySettingInputHeader extends StatelessWidget {
   const GlobalWeekDaySettingInputHeader({super.key});
 
   @override
-  Widget build(BuildContext context) => Row(
+  Widget build(BuildContext context) => const Row(
         children: [
-          'Target work time per week',
-          '',
-          '',
-          '',
-        ]
-            .map((header) => Expanded(
-                    child: Center(
-                  child: Text(
-                    header,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                )))
-            .toList(),
+          Expanded(
+            child: HeaderRow(
+              headers: {
+                'Target work time per week': '',
+              },
+            ),
+          ),
+          Spacer(flex: 3),
+        ],
       );
 }
--- work_time_table_mobile/lib/components/time_input/week_day_setting_input_header.dart
+++ work_time_table_mobile/lib/components/time_input/week_day_setting_input_header.dart
@@ -1,25 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:work_time_table_mobile/components/time_input/header_row.dart';
 
 class WeekDaySettingInputHeader extends StatelessWidget {
   const WeekDaySettingInputHeader({super.key});
 
   @override
-  Widget build(BuildContext context) => Row(
-        children: [
-          'Hour equivalent',
-          'Mandatory work time',
-          'Usual work time',
-          'Usual break duration',
-        ]
-            .map((header) => Expanded(
-                    child: Center(
-                  child: Text(
-                    header,
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                )))
-            .toList(),
+  Widget build(BuildContext context) => const HeaderRow(
+        headers: {
+          'Hour equivalent': '',
+          'Mandatory work time': '',
+          'Usual work time': '',
+          'Usual break duration': '',
+        },
       );
 }
