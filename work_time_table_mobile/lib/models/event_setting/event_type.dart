3f9f890420a8767eb32d0d5b3be2290e1f664c92
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-12-08 20:16:13 +0100

    feat(EventType): color

--- work_time_table_mobile/lib/models/event_setting/event_type.dart
+++ work_time_table_mobile/lib/models/event_setting/event_type.dart
@@ -1,12 +1,39 @@
-enum EventType {
-  publicHoliday(priority: 0, displayValue: 'Public holiday'),
-  vacation(priority: 4, displayValue: 'Vacation'),
-  sickDay(priority: 1, displayValue: 'Sick day'),
-  dayOff(priority: 2, displayValue: 'Day off'),
-  businessTrip(priority: 3, displayValue: 'Business trip');
+import 'dart:ui';
 
-  const EventType({required this.priority, required this.displayValue});
+enum EventType {
+  publicHoliday(
+    priority: 0,
+    displayValue: 'Public holiday',
+    color: Color.fromARGB(255, 145, 216, 78),
+  ),
+  vacation(
+    priority: 4,
+    displayValue: 'Vacation',
+    color: Color.fromARGB(255, 102, 152, 54),
+  ),
+  sickDay(
+    priority: 1,
+    displayValue: 'Sick day',
+    color: Color.fromARGB(255, 152, 54, 54),
+  ),
+  dayOff(
+    priority: 2,
+    displayValue: 'Day off',
+    color: Color.fromARGB(255, 64, 190, 179),
+  ),
+  businessTrip(
+    priority: 3,
+    displayValue: 'Business trip',
+    color: Color.fromARGB(255, 64, 66, 190),
+  );
+
+  const EventType({
+    required this.priority,
+    required this.displayValue,
+    required this.color,
+  });
 
   final int priority;
   final String displayValue;
+  final Color color;
 }
