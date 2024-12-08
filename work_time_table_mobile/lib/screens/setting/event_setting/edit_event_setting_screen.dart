cd3cdcda6ba27f3d1571e7fcd9f4ec42c909c721
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-12-08 15:01:58 +0100

    feat: EditEventSettingScreen

--- work_time_table_mobile/lib/screens/setting/event_setting/edit_event_setting_screen.dart
+++ work_time_table_mobile/lib/screens/setting/event_setting/edit_event_setting_screen.dart
@@ -1,4 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:work_time_table_mobile/blocs/event_setting_cubit.dart';
+import 'package:work_time_table_mobile/components/event_setting/event_setting_editor.dart';
+import 'package:work_time_table_mobile/components/page_template.dart';
+import 'package:work_time_table_mobile/daos/current_user_dao.dart';
+import 'package:work_time_table_mobile/daos/event_setting_dao.dart';
+import 'package:work_time_table_mobile/daos/user_dao.dart';
+import 'package:work_time_table_mobile/models/event_setting/event_setting.dart';
+import 'package:work_time_table_mobile/services/event_setting_service.dart';
+import 'package:work_time_table_mobile/services/user_service.dart';
+import 'package:work_time_table_mobile/stream_helpers/context/context_dependent_value.dart';
 
 class EditEventSettingScreen extends StatelessWidget {
   const EditEventSettingScreen({super.key, this.eventId});
@@ -6,8 +18,42 @@ class EditEventSettingScreen extends StatelessWidget {
   final int? eventId;
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+  Widget build(BuildContext context) => PageTemplate(
+        title: 'Event Setting',
+        content: BlocProvider(
+          create: (context) => EventSettingCubit(EventSettingService(
+            UserService(
+              context.read<UserDao>(),
+              context.read<CurrentUserDao>(),
+            ),
+            context.read<EventSettingDao>(),
+          )),
+          child: BlocBuilder<EventSettingCubit,
+              ContextDependentValue<List<EventSetting>>>(
+            builder: (context, state) => switch (state) {
+              NoContextValue<List<EventSetting>>() => const Center(
+                  child: Text('No user selected'),
+                ),
+              ContextValue<List<EventSetting>>(value: var value) =>
+                EventSettingEditor(
+                  initialValue: value
+                      .where((element) => element.id == eventId)
+                      .firstOrNull,
+                  onCancel: context.pop,
+                  onSubmit: (value) async {
+                    if (eventId == null) {
+                      await context.read<EventSettingCubit>().addEvent(value);
+                    } else {
+                      await context
+                          .read<EventSettingCubit>()
+                          .updateEvent(value);
+                    }
+                    if (!context.mounted) return;
+                    context.pop();
+                  },
+                ),
+            },
+          ),
+        ),
+      );
 }
