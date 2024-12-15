7583ef8f5416d5a5c63f19227ca7546426e5e2e5
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-12-15 14:01:59 +0100

    feat(evaluated_event_setting): implement Identifiable

--- work_time_table_mobile/lib/models/event_setting/evaluated_event_setting.dart
+++ work_time_table_mobile/lib/models/event_setting/evaluated_event_setting.dart
@@ -1,6 +1,7 @@
 import 'package:work_time_table_mobile/models/event_setting/event_setting.dart';
+import 'package:work_time_table_mobile/stream_helpers/identifiable.dart';
 
-class EvaluatedEventSetting {
+class EvaluatedEventSetting implements Identifiable {
   final EventSetting eventSetting;
   final bool firstHalf;
   final bool secondHalf;
@@ -10,4 +11,7 @@ class EvaluatedEventSetting {
     required this.firstHalf,
     required this.secondHalf,
   });
+
+  @override
+  get identity => eventSetting.identity;
 }
