5bc8935bfb6294d5fc2356dac05fa766800af85a
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-12-19 20:00:04 +0100

    feat(DayInputCard): improve Styling

--- work_time_table_mobile/lib/components/time_input/day_input_card.dart
+++ work_time_table_mobile/lib/components/time_input/day_input_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'package:work_time_table_mobile/components/time_input/day_mode_selector.dart';
 import 'package:work_time_table_mobile/components/time_input/time_input_button.dart';
 import 'package:work_time_table_mobile/components/time_input/time_span_input.dart';
@@ -21,141 +22,150 @@ class DayInputCard extends StatelessWidget {
   final void Function(DayValue dayValue) onChange;
 
   @override
-  Widget build(BuildContext context) => Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(32),
-          side: BorderSide(color: Colors.grey.shade400),
-        ),
-        color: Colors.grey.shade300,
-        elevation: 8,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text(
-              displayDateFormat.format(dayValue.date),
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            Text(
-              DayOfWeek.fromDateTime(dayValue.date).name,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
-            const SizedBox(height: 32),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  width: 200,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.end,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 8),
-                        child: Text(
-                          'Work time',
+  Widget build(BuildContext context) => Padding(
+        padding: const EdgeInsets.all(8),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(32),
+            side: BorderSide(
+                color: (isSameDay(dayValue.date, DateTime.now())
+                    ? Colors.yellow.shade400
+                    : Colors.grey.shade400)),
+          ),
+          color: isSameDay(dayValue.date, DateTime.now())
+              ? Colors.yellow.shade300
+              : Colors.grey.shade300,
+          elevation: 8,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                displayDateFormat.format(dayValue.date),
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              Text(
+                DayOfWeek.fromDateTime(dayValue.date).name,
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 32),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: Text(
+                            'Work time',
+                            style: Theme.of(context).textTheme.titleMedium,
+                          ),
+                        ),
+                        TimeSpanInput(
+                          value: (
+                            start: dayValue.workTimeStart.toTimeOfDay(),
+                            end: dayValue.workTimeEnd.toTimeOfDay()
+                          ),
+                          startMax:
+                              settings.mandatoryWorkTimeStart.toTimeOfDay(),
+                          endMin: settings.mandatoryWorkTimeEnd.toTimeOfDay(),
+                          onChange: (start, end) => onChange(DayValue(
+                            date: dayValue.date,
+                            firstHalfMode: dayValue.firstHalfMode,
+                            secondHalfMode: dayValue.secondHalfMode,
+                            workTimeStart: start.toInt(),
+                            workTimeEnd: end.toInt(),
+                            breakDuration: dayValue.breakDuration,
+                          )),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  SizedBox(
+                    width: 200,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Break Duration',
                           style: Theme.of(context).textTheme.titleMedium,
                         ),
-                      ),
-                      TimeSpanInput(
-                        value: (
-                          start: dayValue.workTimeStart.toTimeOfDay(),
-                          end: dayValue.workTimeEnd.toTimeOfDay()
+                        TimeInputButton(
+                          value: dayValue.breakDuration.toTimeOfDay(),
+                          onChange: (value) => onChange(DayValue(
+                            date: dayValue.date,
+                            firstHalfMode: dayValue.firstHalfMode,
+                            secondHalfMode: dayValue.secondHalfMode,
+                            workTimeStart: dayValue.workTimeStart,
+                            workTimeEnd: dayValue.workTimeEnd,
+                            breakDuration: value.toInt(),
+                          )),
                         ),
-                        startMax: settings.mandatoryWorkTimeStart.toTimeOfDay(),
-                        endMin: settings.mandatoryWorkTimeEnd.toTimeOfDay(),
-                        onChange: (start, end) => onChange(DayValue(
-                          date: dayValue.date,
-                          firstHalfMode: dayValue.firstHalfMode,
-                          secondHalfMode: dayValue.secondHalfMode,
-                          workTimeStart: start.toInt(),
-                          workTimeEnd: end.toInt(),
-                          breakDuration: dayValue.breakDuration,
-                        )),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(width: 16),
-                SizedBox(
-                  width: 200,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'Break Duration',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      TimeInputButton(
-                        value: dayValue.breakDuration.toTimeOfDay(),
-                        onChange: (value) => onChange(DayValue(
-                          date: dayValue.date,
-                          firstHalfMode: dayValue.firstHalfMode,
-                          secondHalfMode: dayValue.secondHalfMode,
-                          workTimeStart: dayValue.workTimeStart,
-                          workTimeEnd: dayValue.workTimeEnd,
-                          breakDuration: value.toInt(),
-                        )),
-                      ),
-                    ],
+                ],
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(
+                    width: 200,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Forenoon',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        DayModeSelector(
+                          dayMode: dayValue.firstHalfMode,
+                          onChange: (dayMode) => onChange(DayValue(
+                            date: dayValue.date,
+                            firstHalfMode: dayMode,
+                            secondHalfMode: dayValue.secondHalfMode,
+                            workTimeStart: dayValue.workTimeStart,
+                            workTimeEnd: dayValue.workTimeEnd,
+                            breakDuration: dayValue.breakDuration,
+                          )),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                  width: 200,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'Forenoon',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      DayModeSelector(
-                        dayMode: dayValue.firstHalfMode,
-                        onChange: (dayMode) => onChange(DayValue(
-                          date: dayValue.date,
-                          firstHalfMode: dayMode,
-                          secondHalfMode: dayValue.secondHalfMode,
-                          workTimeStart: dayValue.workTimeStart,
-                          workTimeEnd: dayValue.workTimeEnd,
-                          breakDuration: dayValue.breakDuration,
-                        )),
-                      ),
-                    ],
+                  const SizedBox(width: 16),
+                  SizedBox(
+                    width: 200,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Text(
+                          'Afternoon',
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        DayModeSelector(
+                          dayMode: dayValue.secondHalfMode,
+                          onChange: (dayMode) => onChange(DayValue(
+                            date: dayValue.date,
+                            firstHalfMode: dayValue.firstHalfMode,
+                            secondHalfMode: dayMode,
+                            workTimeStart: dayValue.workTimeStart,
+                            workTimeEnd: dayValue.workTimeEnd,
+                            breakDuration: dayValue.breakDuration,
+                          )),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                const SizedBox(width: 16),
-                SizedBox(
-                  width: 200,
-                  child: Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Text(
-                        'Afternoon',
-                        style: Theme.of(context).textTheme.titleMedium,
-                      ),
-                      DayModeSelector(
-                        dayMode: dayValue.secondHalfMode,
-                        onChange: (dayMode) => onChange(DayValue(
-                          date: dayValue.date,
-                          firstHalfMode: dayValue.firstHalfMode,
-                          secondHalfMode: dayMode,
-                          workTimeStart: dayValue.workTimeStart,
-                          workTimeEnd: dayValue.workTimeEnd,
-                          breakDuration: dayValue.breakDuration,
-                        )),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       );
 }
