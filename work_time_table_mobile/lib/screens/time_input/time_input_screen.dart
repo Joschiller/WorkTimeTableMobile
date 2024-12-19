f908499bec7856ff647e40c74cbdd9ca96080b04
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-12-19 20:32:06 +0100

    feat: TimeInputScreen Layout

--- work_time_table_mobile/lib/screens/time_input/time_input_screen.dart
+++ work_time_table_mobile/lib/screens/time_input/time_input_screen.dart
@@ -1,11 +1,163 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:work_time_table_mobile/components/page_template.dart';
+import 'package:work_time_table_mobile/components/time_input/day_input_card.dart';
+import 'package:work_time_table_mobile/constants/routes.dart';
+import 'package:work_time_table_mobile/models/value/day_mode.dart';
+import 'package:work_time_table_mobile/models/value/day_value.dart';
+import 'package:work_time_table_mobile/models/week_setting/day_of_week.dart';
+import 'package:work_time_table_mobile/models/week_setting/week_day_setting.dart';
+import 'package:work_time_table_mobile/utils.dart';
 
-class TimeInputScreen extends StatelessWidget {
-  const TimeInputScreen({super.key});
+class TimeInputScreen extends StatefulWidget {
+  TimeInputScreen({super.key});
+
+  final todayKey = GlobalKey();
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  State<TimeInputScreen> createState() => _TimeInputScreenState();
+}
+
+class _TimeInputScreenState extends State<TimeInputScreen> {
+  late DateTime selectedDay;
+
+  DateTime get _weekOfToday => DateTime.now().toDay().firstDayOfWeek;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollToToday();
   }
+
+  void _scrollToToday() {
+    setState(() {
+      selectedDay = _weekOfToday;
+    });
+    // small delay, to ensure, that the target date has been drawn
+    Future.delayed(const Duration(milliseconds: 100)).then(
+      (value) {
+        if (widget.todayKey.currentContext != null) {
+          Scrollable.ensureVisible(
+            widget.todayKey.currentContext!,
+            duration: const Duration(seconds: 2),
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) => PageTemplate(
+        title:
+            '${displayDateFormat.format(selectedDay)} - ${displayDateFormat.format(
+          selectedDay.add(const Duration(days: 6)),
+        )}',
+        menuButtons: [
+          (
+            onPressed: () => showDatePicker(
+                  context: context,
+                  initialDate: selectedDay,
+                  firstDate: DateTime.utc(2020, 1, 1),
+                  lastDate: DateTime.utc(DateTime.now().year + 5, 12, 31),
+                ).then(
+                  (value) {
+                    if (value != null) {
+                      setState(() => selectedDay = value.firstDayOfWeek);
+                    }
+                  },
+                ),
+            icon: const Icon(Icons.calendar_month),
+          ),
+          (
+            onPressed: () => SettingsScreenRoute().push(context),
+            icon: const Icon(Icons.settings),
+          ),
+        ],
+        content: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.grey.shade600,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      // TODO: disable inputs, if the week cannot be edited anymore
+                      ...DayOfWeek.values.map((dayOfWeek) => DayInputCard(
+                            key: isSameDay(
+                                    selectedDay
+                                        .add(Duration(days: dayOfWeek.index)),
+                                    DateTime.now())
+                                ? widget.todayKey
+                                : null,
+                            // TODO: load settings
+                            settings: WeekDaySetting(
+                              dayOfWeek: dayOfWeek,
+                              timeEquivalent: 0,
+                              mandatoryWorkTimeStart: 0,
+                              mandatoryWorkTimeEnd: 0,
+                              defaultWorkTimeStart: 0,
+                              defaultWorkTimeEnd: 0,
+                              defaultBreakDuration: 0,
+                            ),
+                            // TODO: load values
+                            dayValue: DayValue(
+                              date: selectedDay
+                                  .add(Duration(days: dayOfWeek.index)),
+                              breakDuration: 0,
+                              firstHalfMode: DayMode.workDay,
+                              secondHalfMode: DayMode.workDay,
+                              workTimeEnd: 0,
+                              workTimeStart: 0,
+                            ),
+                            onChange: (dayValue) {
+                              // TODO: instantly persist changes whenever a value is altered
+                            },
+                          ))
+                      // TODO: if the week can be closed, show an additional card at the end for closing the week
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              color: Colors.grey.shade300,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => setState(() => selectedDay =
+                          selectedDay.subtract(const Duration(days: 7))),
+                      child: const Row(
+                        children: [
+                          Icon(Icons.arrow_left),
+                          Text('Previous Week'),
+                          SizedBox(width: 16),
+                        ],
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: _scrollToToday,
+                      child: const Text('Today'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () => setState(() => selectedDay =
+                          selectedDay.add(const Duration(days: 7))),
+                      child: const Row(
+                        children: [
+                          SizedBox(width: 16),
+                          Text('Next Week'),
+                          Icon(Icons.arrow_right),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
 }
