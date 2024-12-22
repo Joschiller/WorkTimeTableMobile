31cb685b2f805c1a0135a167ade124162edbdaff
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-12-22 14:29:36 +0100

    feat: TimeInputSummary

---
+++ work_time_table_mobile/lib/components/time_input/time_input_summary.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TimeInputSummary extends StatelessWidget {
+  const TimeInputSummary({
+    super.key,
+    required this.label,
+    required this.duration,
+  });
+
+  final String label;
+  final int duration;
+
+  @override
+  Widget build(BuildContext context) => Container(
+        color: Colors.grey.shade300,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    label,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text('${duration ~/ 60}:${duration.abs() % 60} h'),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+}
