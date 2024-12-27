90447e3b27b2907f64c674ba0a4ea41e3ab7742b
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-12-27 11:37:15 +0100

    feat(SettingsScreen): rudimentary routes

--- work_time_table_mobile/lib/screens/settings_screen.dart
+++ work_time_table_mobile/lib/screens/settings_screen.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:work_time_table_mobile/components/page_template.dart';
+import 'package:work_time_table_mobile/constants/routes.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
+  Widget build(BuildContext context) => PageTemplate(
+        title: 'Settings',
+        content: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () => UserScreenRoute().push(context),
+                child: const Text('User')),
+            ElevatedButton(
+                onPressed: () => WeekSettingScreenRoute().push(context),
+                child: const Text('Week Settings')),
+            ElevatedButton(
+                onPressed: () => EventSettingScreenRoute().push(context),
+                child: const Text('Event Settings')),
+          ],
+        ),
+      );
 }
