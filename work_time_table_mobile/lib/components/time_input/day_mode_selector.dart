796054cdcf3504bd30551630f1bef58cf6428046
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2024-12-27 11:15:31 +0100

    fix(day_mode_selector): hide nonWorkDay

--- work_time_table_mobile/lib/components/time_input/day_mode_selector.dart
+++ work_time_table_mobile/lib/components/time_input/day_mode_selector.dart
@@ -14,12 +14,14 @@ class DayModeSelector extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => DropdownButton<String>(
-        value: dayMode.name,
+        value: dayMode != DayMode.nonWorkDay ? dayMode.name : null,
+        hint: Text(dayMode.displayValue),
         onChanged: onChange != null
             ? (String? value) => onChange?.call(
                 DayMode.values.firstWhere((element) => element.name == value))
             : null,
         items: DayMode.values
+            .where((element) => element != DayMode.nonWorkDay)
             .map<DropdownMenuItem<String>>((value) => DropdownMenuItem<String>(
                   value: value.name,
                   child: Text(value.displayValue),
