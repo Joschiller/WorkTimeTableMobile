aaa5ddd1c8641337c9fb7d0c4dd08c4a6d4844ca
Joachim Schiller <83116042+Joschiller@users.noreply.github.com>
2025-02-14 14:19:30 +0100

    feat(StatisticsSummary): half work day handling

--- work_time_table_mobile/lib/components/statistics/statistics_summary.dart
+++ work_time_table_mobile/lib/components/statistics/statistics_summary.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:work_time_table_mobile/components/statistics/statistics_mode_selector.dart';
 import 'package:work_time_table_mobile/models/statistics/statistics_mode.dart';
 import 'package:work_time_table_mobile/models/statistics/statistics_state.dart';
+import 'package:work_time_table_mobile/models/value/day_mode.dart';
 import 'package:work_time_table_mobile/models/value/day_value.dart';
 import 'package:work_time_table_mobile/models/week_setting/day_of_week.dart';
 import 'package:work_time_table_mobile/services/analyze_list.dart';
@@ -17,19 +18,41 @@ class StatisticsSummary extends StatefulWidget {
 
 class _StatisticsSummaryState extends State<StatisticsSummary> {
   var _statisticsMode = StatisticsMode.average;
+  var _includeHalfWorkDays = false;
 
   @override
   Widget build(BuildContext context) => Column(
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          StatisticsModeSelector(
-            statisticsMode: _statisticsMode,
-            onChange: (statisticsMode) => setState(
-              () {
-                _statisticsMode = statisticsMode;
-              },
-            ),
+          Row(
+            children: [
+              Expanded(
+                child: StatisticsModeSelector(
+                  statisticsMode: _statisticsMode,
+                  onChange: (statisticsMode) => setState(
+                    () {
+                      _statisticsMode = statisticsMode;
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Row(
+                  children: [
+                    Switch(
+                      value: _includeHalfWorkDays,
+                      onChanged: (value) => setState(() {
+                        _includeHalfWorkDays = value;
+                      }),
+                    ),
+                    const Expanded(
+                      child: Text('Include Half Work Days'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
           Row(
             children: [
@@ -72,50 +95,63 @@ class _StatisticsSummaryState extends State<StatisticsSummary> {
                 const SizedBox(width: 16),
                 Expanded(
                   flex: 4,
-                  child: !widget.statistics.dayValuesPerDayOfWeek.values
-                          .any((v) => v.isNotEmpty)
-                      ? const Text('No Data')
-                      : Row(
-                          children: [
-                            const Text('Work Period: '),
-                            Text(
-                              _durationToString(Duration(
-                                minutes: analyzeList<DayValue>(
-                                      widget.statistics
-                                          .dayValuesPerDayOfWeek[day],
-                                      (item) => item.workTimeStart.toDouble(),
-                                      _statisticsMode,
-                                    )?.toInt() ??
-                                    0,
-                              )),
-                            ),
-                            const Text(' - '),
-                            Text(
-                              _durationToString(Duration(
-                                minutes: analyzeList<DayValue>(
-                                      widget.statistics
-                                          .dayValuesPerDayOfWeek[day],
-                                      (item) => item.workTimeEnd.toDouble(),
-                                      _statisticsMode,
-                                    )?.toInt() ??
-                                    0,
-                              )),
-                            ),
-                            const SizedBox(width: 16),
-                            const Text('Break Duration: '),
-                            Text(
-                              _durationToString(Duration(
-                                minutes: analyzeList<DayValue>(
-                                      widget.statistics
-                                          .dayValuesPerDayOfWeek[day],
-                                      (item) => item.breakDuration.toDouble(),
-                                      _statisticsMode,
-                                    )?.toInt() ??
-                                    0,
-                              )),
-                            ),
-                          ],
+                  child: Builder(builder: (context) {
+                    final relevantDaysForDayOfWeek = widget
+                        .statistics.dayValuesPerDayOfWeek[day]
+                        ?.where((d) {
+                      if (d.firstHalfMode == DayMode.workDay &&
+                          d.secondHalfMode == DayMode.workDay) {
+                        return true;
+                      }
+                      if (d.firstHalfMode == DayMode.workDay ||
+                          d.secondHalfMode == DayMode.workDay) {
+                        return _includeHalfWorkDays;
+                      }
+                      return false;
+                    });
+                    if (relevantDaysForDayOfWeek == null ||
+                        relevantDaysForDayOfWeek.isEmpty) {
+                      return const Text('No Data');
+                    }
+                    return Row(
+                      children: [
+                        const Text('Work Period: '),
+                        Text(
+                          _durationToString(Duration(
+                            minutes: analyzeList<DayValue>(
+                                  relevantDaysForDayOfWeek,
+                                  (item) => item.workTimeStart.toDouble(),
+                                  _statisticsMode,
+                                )?.toInt() ??
+                                0,
+                          )),
                         ),
+                        const Text(' - '),
+                        Text(
+                          _durationToString(Duration(
+                            minutes: analyzeList<DayValue>(
+                                  relevantDaysForDayOfWeek,
+                                  (item) => item.workTimeEnd.toDouble(),
+                                  _statisticsMode,
+                                )?.toInt() ??
+                                0,
+                          )),
+                        ),
+                        const SizedBox(width: 16),
+                        const Text('Break Duration: '),
+                        Text(
+                          _durationToString(Duration(
+                            minutes: analyzeList<DayValue>(
+                                  relevantDaysForDayOfWeek,
+                                  (item) => item.breakDuration.toDouble(),
+                                  _statisticsMode,
+                                )?.toInt() ??
+                                0,
+                          )),
+                        ),
+                      ],
+                    );
+                  }),
                 ),
               ],
             ),
